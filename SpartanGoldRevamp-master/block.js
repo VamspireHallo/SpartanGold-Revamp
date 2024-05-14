5f4f4bc309fa68c9677fd3134e7dc585c295fae4
VK Vusuwandla <55940072+VamspireHallo@users.noreply.github.com>
2024-05-14 13:08:44 -0700

    Update block.js

--- SpartanGoldRevamp-master/block.js
+++ SpartanGoldRevamp-master/block.js
@@ -4,7 +4,6 @@ const Blockchain = require('./blockchain.js');
 
 const utils = require('./utils.js');
 const { MerkleTree } = require('./MerkleTree.js');
-
 /**
  * A block is a collection of transactions, with a hash connecting it
  * to a previous block.
@@ -22,14 +21,15 @@ module.exports = class Block {
    *      produces a smaller value when hashed.
    * @param {Number} [coinbaseReward] - The gold that a miner earns for finding a block proof.
    */
-  constructor(rewardAddr, prevBlock, target=Blockchain.POW_TARGET, coinbaseReward=Blockchain.COINBASE_AMT_ALLOWED) {
+  constructor(rewardAddr, prevBlock, target = Blockchain.POW_TARGET, coinbaseReward = Blockchain.COINBASE_AMT_ALLOWED) {
     this.prevBlockHash = prevBlock ? prevBlock.hashVal() : null;
     this.target = target;
-
-    // Get the balances and nonces from the previous block, if available.
-    // Note that balances and nonces are NOT part of the serialized format.
     this.balances = prevBlock ? new Map(prevBlock.balances) : new Map();
     this.nextNonce = prevBlock ? new Map(prevBlock.nextNonce) : new Map();
+    this.chainLength = prevBlock ? prevBlock.chainLength + 1 : 0;
+    this.timestamp = Date.now();
+    this.rewardAddr = rewardAddr;
+    this.coinbaseReward = coinbaseReward;
 
     if (prevBlock && prevBlock.rewardAddr) {
       // Add the previous block's rewards to the miner who found the proof.
@@ -37,39 +37,21 @@ module.exports = class Block {
       this.balances.set(prevBlock.rewardAddr, winnerBalance + prevBlock.totalRewards());
     }
 
-    // Storing transactions in a Map to preserve key order.
-    this.transactions = new MerkleTree([]);
-
-    // Adding toJSON methods for transactions and balances, which help with
-    // serialization.
-    // this.transactions.toJSON = () => {
-    //   return JSON.stringify(Array.from(this.transactions.entries()));
-    // }
-    // this.balances.toJSON = () => {
-    //   return JSON.stringify(Array.from(this.balances.entries()));
-    // }
-
-
-    // Used to determine the winner between competing chains.
-    // Note that this is a little simplistic -- an attacker
-    // could make a long, but low-work chain.  However, this works
-    // well enough for us.
-    this.chainLength = prevBlock ? prevBlock.chainLength+1 : 0;
-
-    this.timestamp = Date.now();
-
-    // The address that will gain both the coinbase reward and transaction fees,
-    // assuming that the block is accepted by the network.
-    this.rewardAddr = rewardAddr;
-
-    this.coinbaseReward = coinbaseReward;
-
-    //Initializing the current block size
+    this.transactions = new MerkleTree();
+    
     this.currentBlockSize = 0;
-
   }
 
+  /**
+   * Determines whether the block is the beginning of the chain.
+   *
+   * @returns {Boolean} - True if this is the first block in the chain.
+   */
+  isGenesisBlock() {
+    return this.chainLength === 0;
+  }
 
+  
   /**
    * Adds the block to the blockchain by calling the addBlock method.
    *
@@ -85,30 +67,6 @@ module.exports = class Block {
     return blockchain.addBlock(this);
   }
 
-
-
-
-  /**
-   * Determines whether the block is the beginning of the chain.
-   *
-   * @returns {Boolean} - True if this is the first block in the chain.
-   */
-  isGenesisBlock() {
-    return this.chainLength === 0;
-  }
-
-  /**
-   * Returns true if the hash of the block is less than the target
-   * proof of work value.
-   *
-   * @returns {Boolean} - True if the block has a valid proof.
-   */
-  hasValidProof() {
-    let h = utils.hash(this.serialize());
-    let n = BigInt(`0x${h}`);
-    return n < this.target;
-  }
-
   /**
    * Converts a Block into string form.  Some fields are deliberately omitted.
    * Note that Block.deserialize plus block.rerun should restore the block.
@@ -117,6 +75,21 @@ module.exports = class Block {
    */
   serialize() {
     return JSON.stringify(this);
+  }
+
+/**
+   * Returns true if the hash of the block is less than the target
+   * proof of work value.
+   *
+   * @returns {Boolean} - True if the block has a valid proof.
+   */
+hasValidProof() {
+  let h = utils.hash(this.serialize());
+  let n = BigInt(`0x${h}`);
+  return n < this.target;
+}
+
+
    //if (this.isGenesisBlock()) {
    //  // The genesis block does not contain a proof or transactions,
    //  // but is the only block than can specify balances.
@@ -154,20 +127,18 @@ module.exports = class Block {
    //  return JSON.stringify(o, ['chainLength', 'timestamp', 'transactions',
    //       'prevBlockHash', 'proof', 'rewardAddr']);
    //}
-  }
+  
 
-  toJSON() {
+   toJSON() {
     let o = {
       chainLength: this.chainLength,
       timestamp: this.timestamp,
     };
     if (this.isGenesisBlock()) {
-      // The genesis block does not contain a proof or transactions,
-      // but is the only block than can specify balances.
       o.balances = Array.from(this.balances.entries());
     } else {
-      o.transactions = this.transactions;
       o.prevBlockHash = this.prevBlockHash;
+      o.transactions = this.transactions;
       o.proof = this.proof;
       o.rewardAddr = this.rewardAddr;
     }
@@ -206,17 +177,9 @@ module.exports = class Block {
 
     const txSize = Buffer.byteLength(JSON.stringify(tx));
     
-    if(this.transactions.hasTx(tx)){
-      if(client){
-        client.log("Duplicate transaction found");
-        return false;
-      }
-    }
-
     console.log(`Checking current block size, ${this.currentBlockSize + txSize} compared to max block size, ${Blockchain.MAX_BLOCK_SIZE_BYTES}`);
     if (this.currentBlockSize + txSize > Blockchain.MAX_BLOCK_SIZE_BYTES) {
-      if (client) 
-        client.log(`Transaction ${tx.id} exceeds block size limit.`);
+      if (client) client.log(`Transaction ${tx.id} exceeds block size limit.`);
       return false;
     }
 
@@ -227,10 +190,7 @@ module.exports = class Block {
       if (client) client.log(`Signature undefined ${tx.id}.`);
       return false;
     }
-    else if (tx.sig === undefined) {
-      if (client) client.log(`Unsigned transaction ${tx.id}.`);
-      return false;
-    } else if (!tx.validSignature()) {
+    else if (!tx.validSignature()) {
       if (client) client.log(`Invalid signature for transaction ${tx.id}.`);
       return false;
     } else if (!tx.sufficientFunds(this)) {
@@ -238,34 +198,42 @@ module.exports = class Block {
       return false;
     }
     
+    if (this.transactions.hasTransaction(tx)) {
+      if (client) client.log(`Duplicate transaction ${tx.id}.`);
+      return false;
+    }
 
-
-  
+    // Checking and updating nonce value.
+    // This portion prevents replay attacks.
     let nonce = this.nextNonce.get(tx.from) || 0;
     if (tx.nonce < nonce) {
       if (client) client.log(`Replayed transaction ${tx.id}.`);
       return false;
     } else if (tx.nonce > nonce) {
+      // FIXME: Need to do something to handle this case more gracefully.
       if (client) client.log(`Out of order transaction ${tx.id}.`);
       return false;
     } else {
       this.nextNonce.set(tx.from, nonce + 1);
     }
 
-    this.transactions.addTxn(tx);
+    // Adding the transaction to the block
+    this.transactions.addtxn(tx);
 
-    updateBalances(tx);
-
-
-/*
-    const leaves = this.transactions;
-    this.merkleTree = new MerkleTree(leaves); 
     this.updateBalances(tx);
-*/
+
     return true;
   }
 
+  updateBalances(tx) {
+    let senderBalance = this.balanceOf(tx.from);
+    this.balances.set(tx.from, senderBalance - tx.totalOutput());
 
+    tx.outputs.forEach(({ amount, address }) => {
+        let oldBalance = this.balanceOf(address);
+        this.balances.set(address, amount + oldBalance);
+    });
+  }
 
   /**
    * When a block is received from another party, it does not include balances or a record of
@@ -279,21 +247,18 @@ module.exports = class Block {
    * @returns {Boolean} - True if the block's transactions are all valid.
    */
   rerun(prevBlock) {
-    // Setting balances to the previous block's balances.
     this.balances = new Map(prevBlock.balances);
     this.nextNonce = new Map(prevBlock.nextNonce);
 
-    // Adding coinbase reward for prevBlock.
     let winnerBalance = this.balanceOf(prevBlock.rewardAddr);
     if (prevBlock.rewardAddr) this.balances.set(prevBlock.rewardAddr, winnerBalance + prevBlock.totalRewards());
 
-    let txns = this.transactions.getTxns();
+    let txs = this.transactions.getTransactions();
     this.transactions = new MerkleTree();
     for (let tx of txs) {
       let success = this.addTransaction(tx);
-      if (!success) return false; 
+      if (!success) return false;
     }
-    
 
     return true;
   }
@@ -327,7 +292,6 @@ module.exports = class Block {
       totalFee += tx.fee;
     }
     return this.coinbaseReward+ totalFee;
-   // return transactions.reduce((reward, tx) => reward + tx.fee, this.coinbaseReward);
   }
 
   /**
@@ -343,20 +307,6 @@ module.exports = class Block {
     if(!this.transactions){
       return false;
     }
-    return this.transactions.hasTransaction(tx) !== null;
+    return this.transactions.hasTransaction(tx) !== null;  
   }
-
-
-  updateBalances(tx) {
-    let senderBalance = this.balanceOf(tx.from);
-    this.balances.set(tx.from, senderBalance - tx.totalOutput());
-
-    tx.outputs.forEach(({ amount, address }) => {
-        let oldBalance = this.balanceOf(address);
-        this.balances.set(address, amount + oldBalance);
-    });
-  }
-
-
 };
-
